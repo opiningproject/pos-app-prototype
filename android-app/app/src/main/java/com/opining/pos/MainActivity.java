12b85ee1e5bc5c6e4f5dbafbb2159c42c00f4b6a
opiningdevelop <Opiningdevelop@gmail.com>
2026-09-03 17:13:21 +0200

    updated base url

--- android-app/app/src/main/java/com/opining/pos/MainActivity.java
+++ android-app/app/src/main/java/com/opining/pos/MainActivity.java
@@ -23,7 +23,7 @@ import com.sunmi.peripheral.printer.SunmiPrinterService;
 public class MainActivity extends Activity {
 
     private static final String LOCAL_URL = "file:///android_asset/index.html";
-    private static final String ONLINE_URL = "https://admin.dryfftjwieiwjw.online/";
+    private static final String ONLINE_URL = "https://admin.opiningstore.com/";
 
     private WebView webView;
     private SunmiPrinterService printerService;
